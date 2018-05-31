22d9cf6475a212c1e6ffecb87c7ef653a31ec886
ChenYong <chenyong@jimeng.io>
2018-05-31 20:04:02 +0800

    trying different parameters

--- src/app/animate.cljs
+++ src/app/animate.cljs
@@ -5,6 +5,6 @@
   (let [tree (:tree @global/*objects)]
     (when (and (some? tree))
       (swap! global/*values assoc :time timestamp)
-      (set! (.. tree -rotation -y) (/ (:time @global/*values) 4000))
+      (comment set! (.. tree -rotation -y) (/ (:time @global/*values) 4000))
       (.render global/renderer global/scene global/camera)))
   (.requestAnimationFrame js/window animation-loop!))
